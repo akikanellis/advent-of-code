ae6e656a3a22becebdc4926f09f580dd2c6a80a2
Aki Kanellis <hello@akikanellis.com>
2023-02-13 20:21:04 +0000

    feat: solve year 2022, day 3

---
+++ advent-of-code/src/main/kotlin/com/akikanellis/adventofcode/year2022/Day03.kt
@@ -0,0 +1,24 @@
+package com.akikanellis.adventofcode.year2022
+
+object Day03 {
+    fun sumOfPrioritiesForMisplacedItemTypes(input: String) = lines(input)
+        .map { Pair(it.take(it.length / 2).toSet(), it.takeLast(it.length / 2).toSet()) }
+        .map { it.first.intersect(it.second).single() }
+        .sumOf { priority(it) }
+
+    fun sumOfPrioritiesForBadges(input: String) = lines(input)
+        .chunked(3)
+        .map { Triple(it[0].toSet(), it[1].toSet(), it[2].toSet()) }
+        .map { it.first.intersect(it.second).intersect(it.third).single() }
+        .sumOf { priority(it) }
+
+    private fun lines(input: String) = input
+        .lines()
+        .filter { it.isNotBlank() }
+
+    private fun priority(itemType: Char) =
+        when {
+            itemType.isLowerCase() -> itemType - 'a' + 1
+            else -> itemType - 'A' + 27
+        }
+}
---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day03Test.kt
@@ -0,0 +1,40 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.utils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+
+class Day03Test {
+    @ParameterizedTest
+    @CsvSource(
+        "/day-03-input-example.txt, 157",
+        "/day-03-input-puzzle.txt,  7_990"
+    )
+    fun `calculates sum of priorities for misplaced item types`(
+        inputFile: String,
+        expectedSumOfPriorities: Int
+    ) {
+        val input = resourceText(inputFile)
+
+        val sumOfPrioritiesForMisplacedItemTypes = Day03.sumOfPrioritiesForMisplacedItemTypes(input)
+
+        assertEquals(expectedSumOfPriorities, sumOfPrioritiesForMisplacedItemTypes)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "/day-03-input-example.txt, 70",
+        "/day-03-input-puzzle.txt,  2_602"
+    )
+    fun `calculates sum of priorities for badges`(
+        inputFile: String,
+        expectedSumOfPriorities: Int
+    ) {
+        val input = resourceText(inputFile)
+
+        val sumOfPrioritiesForBadges = Day03.sumOfPrioritiesForBadges(input)
+
+        assertEquals(expectedSumOfPriorities, sumOfPrioritiesForBadges)
+    }
+}
