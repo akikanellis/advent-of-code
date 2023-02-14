f79f37b14ceda7c0797224d5355ca3bfceddc17c
Aki Kanellis <hello@akikanellis.com>
2023-02-14 19:07:55 +0000

    feat: solve year 2022, day 6

---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day06Test.kt
@@ -0,0 +1,61 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.utils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day06Test {
+    @ParameterizedTest
+    @CsvSource(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb,    7",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz,      5",
+        "nppdvjthqldpwncqszvftbrmjlhg,      6",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg, 10",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw,  11"
+    )
+    fun `finds the position of the first packet marker for examples`(
+        input: String,
+        expectedPosition: Int
+    ) {
+        val firstPacketMarkerPosition = Day06.firstPacketMarkerPosition(input)
+
+        assertEquals(expectedPosition, firstPacketMarkerPosition)
+    }
+
+    @Test
+    fun `finds the position of the first packet marker for puzzle input`() {
+        val input = resourceText("/day-06-input-puzzle.txt")
+
+        val firstPacketMarkerPosition = Day06.firstPacketMarkerPosition(input)
+
+        assertEquals(1_702, firstPacketMarkerPosition)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb,    19",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz,      23",
+        "nppdvjthqldpwncqszvftbrmjlhg,      23",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg, 29",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw,  26"
+    )
+    fun `finds the position of the first message marker for examples`(
+        input: String,
+        expectedPosition: Int
+    ) {
+        val firstMessageMarkerPosition = Day06.firstMessageMarkerPosition(input)
+
+        assertEquals(expectedPosition, firstMessageMarkerPosition)
+    }
+
+    @Test
+    fun `finds the position of the first message marker for puzzle`() {
+        val input = resourceText("/day-06-input-puzzle.txt")
+
+        val firstMessageMarkerPosition = Day06.firstMessageMarkerPosition(input)
+
+        assertEquals(3_559, firstMessageMarkerPosition)
+    }
+}
