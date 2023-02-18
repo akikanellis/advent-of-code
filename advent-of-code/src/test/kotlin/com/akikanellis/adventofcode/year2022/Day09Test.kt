896aa94c6e2117f3ba56aa30aa803004bcb27570
Aki Kanellis <hello@akikanellis.com>
2023-02-18 18:01:22 +0000

    feat: solve year 2022, day 9

---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day09Test.kt
@@ -0,0 +1,27 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.utils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+
+class Day09Test {
+    @ParameterizedTest
+    @CsvSource(
+        "/day-09-part-1-input-example.txt, 2,  13",
+        "/day-09-input-puzzle.txt,         2,  6_044",
+        "/day-09-part-2-input-example.txt, 10, 36",
+        "/day-09-input-puzzle.txt,         10, 2_384"
+    )
+    fun `finds number of positions the tail visited`(
+        inputFile: String,
+        numberOfKnots: Int,
+        expectedNumberOfPositions: Int
+    ) {
+        val input = resourceText(inputFile)
+
+        val numberOfPositionsTailVisited = Day09.numberOfPositionsTailVisited(input, numberOfKnots)
+
+        assertEquals(expectedNumberOfPositions, numberOfPositionsTailVisited)
+    }
+}
