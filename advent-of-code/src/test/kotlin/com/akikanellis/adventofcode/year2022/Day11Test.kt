77324fb81f3ecf0cf1d375a76280e3e1aa2641ec
Aki Kanellis <hello@akikanellis.com>
2023-02-19 20:17:35 +0000

    feat: solve year 2022, day 11 (#32)
    Closes: #27

---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day11Test.kt
@@ -0,0 +1,28 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.utils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+
+class Day11Test {
+    @ParameterizedTest
+    @CsvSource(
+        "/day-11-input-example.txt, 20,     3, 10_605",
+        "/day-11-input-puzzle.txt,  20,     3, 61_503",
+        "/day-11-input-example.txt, 10_000, 1, 2_713_310_158",
+        "/day-11-input-puzzle.txt,  10_000, 1, 14_081_365_540"
+    )
+    fun `calculates monkey business level`(
+        inputFile: String,
+        rounds: Int,
+        itemDivisor: Int,
+        expectedLevel: Long
+    ) {
+        val input = resourceText(inputFile)
+
+        val monkeyBusinessLevel = Day11.monkeyBusinessLevel(input, rounds, itemDivisor)
+
+        assertEquals(expectedLevel, monkeyBusinessLevel)
+    }
+}
