607d0e7b01433cf76892305d0c5b20d9a52c566e
Aki Kanellis <hello@akikanellis.com>
2023-02-19 14:44:20 +0000

    feat: solve year 2022, day 10

--- advent-of-code/src/main/kotlin/com/akikanellis/adventofcode/year2022/Day10.kt
+++ advent-of-code/src/main/kotlin/com/akikanellis/adventofcode/year2022/Day10.kt
@@ -2,34 +2,115 @@ package com.akikanellis.adventofcode.year2022
 
 object Day10 {
     fun sumOfSignalStrengths(input: String): Int {
-        var x = 1
-        var sumOfSignalStrengths = 0
-        var cycle = 0
-
         val cyclesOfInterest = IntProgression.fromClosedRange(20, 220, 40)
 
-        input.lines()
-            .filter { it.isNotBlank() }
-            .forEach { instruction ->
-                cycle++
-                if (cyclesOfInterest.contains(cycle)) sumOfSignalStrengths += (x * cycle)
-
-                if (instruction.startsWith("addx")) {
-                    cycle++
-                    if (cyclesOfInterest.contains(cycle)) sumOfSignalStrengths += (x * cycle)
-                    x += instruction.split(" ")[1].toInt()
-                }
-            }
-
-        return sumOfSignalStrengths
+        return systemSnapshots(input)
+            .filter { cyclesOfInterest.contains(it.cycle) }
+            .sumOf { it.signalStrength }
     }
 
+    fun displayOutput(input: String) = systemSnapshots(input)
+        .last()
+        .crt
+        .displayOutput()
 
-    // for each command
-    // tick++
-    // if tick divisible by 40, add X to sum
-    // if command is addx, increase X and tick++ and if
-    // map to command, cycle count
-    //
+    private fun systemSnapshots(input: String): List<SystemSnapshot> {
+        val instructions = input
+            .lines()
+            .filter { it.isNotBlank() }
+            .map { Instruction(it) }
+            .toMutableList()
 
+        return (1..240)
+            .fold(
+                listOf(
+                    SystemSnapshot(
+                        cycle = 0,
+                        sprite = (0..2),
+                        signalStrength = 0,
+                        crt = Crt(Pair(40, 6))
+                    )
+                )
+            ) { snapshots, cycle ->
+                val lastSnapshot = snapshots.last()
+                val lastSprite = lastSnapshot.sprite
+                val signalStrength = (lastSprite.first + 1) * cycle
+                val crt = lastSnapshot.crt.drawNextPixel(lastSprite)
+
+                val instruction = instructions[0].decreasedCost()
+                instructions[0] = instruction
+
+                val sprite = if (instruction.remainingCost == 0) {
+                    instruction.updatedSprite(lastSprite).also { instructions.removeFirst() }
+                } else {
+                    lastSprite
+                }
+
+                snapshots + SystemSnapshot(
+                    cycle = cycle,
+                    sprite = sprite,
+                    signalStrength = signalStrength,
+                    crt = crt
+                )
+            }
+    }
+
+    private data class SystemSnapshot(
+        val cycle: Int,
+        val sprite: IntRange,
+        val signalStrength: Int,
+        val crt: Crt
+    )
+
+    private data class Instruction(
+        private val instruction: String,
+        val name: String = instruction.split(" ")[0],
+        val startingCost: Int = if (name == "addx") 2 else 1,
+        val remainingCost: Int = startingCost
+    ) {
+        fun decreasedCost() = copy(remainingCost = remainingCost - 1)
+
+        fun updatedSprite(sprite: IntRange) = if (name == "addx") {
+            val valueToIncreaseSpriteBy = instruction.split(" ")[1].toInt()
+            IntRange(
+                sprite.first + valueToIncreaseSpriteBy,
+                sprite.last + valueToIncreaseSpriteBy
+            )
+        } else {
+            sprite
+        }
+    }
+
+    data class Crt(
+        private val size: Pair<Int, Int>,
+        private val drawnPixels: List<Pixel> = emptyList()
+    ) {
+        private val lastDrawnPixel = drawnPixels.lastOrNull()
+
+        fun drawNextPixel(sprite: IntRange): Crt {
+            val nextPixelPosition = nextPixelPosition()
+            val nextPixelCharacter = if (sprite.contains(nextPixelPosition.first)) '#' else '.'
+
+            return copy(drawnPixels = drawnPixels + Pixel(nextPixelPosition, nextPixelCharacter))
+        }
+
+        private fun nextPixelPosition() = when {
+            lastDrawnPixel == null -> Pair(0, 0)
+            reachedTheEdge(lastDrawnPixel) -> Pair(0, lastDrawnPixel.position.second + 1)
+            else -> Pair(lastDrawnPixel.position.first + 1, lastDrawnPixel.position.second)
+        }
+
+        private fun reachedTheEdge(lastDrawnPixel: Pixel) =
+            size.first - lastDrawnPixel.position.first == 1
+
+        fun displayOutput() = drawnPixels
+            .chunked(size.first)
+            .joinToString("\n") { row ->
+                row.joinToString("") { pixel ->
+                    pixel.character.toString()
+                }
+            }
+    }
+
+    data class Pixel(val position: Pair<Int, Int>, val character: Char = ' ')
 }
--- advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day10Test.kt
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day10Test.kt
@@ -3,13 +3,14 @@ package com.akikanellis.adventofcode.year2022
 import com.akikanellis.adventofcode.utils.resourceText
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day10Test {
     @ParameterizedTest
     @CsvSource(
         "/day-10-input-example.txt, 13_140",
-        "/day-10-input-puzzle.txt, 13_140"
+        "/day-10-input-puzzle.txt,  15_220"
     )
     fun `calculates sum of signal strengths`(
         inputFile: String,
@@ -20,5 +21,24 @@ class Day10Test {
         val sumOfSignalStrengths = Day10.sumOfSignalStrengths(input)
 
         assertEquals(expectedSum, sumOfSignalStrengths)
+    }
+
+    @Test
+    fun `shows display output`() {
+        val input = resourceText("/day-10-input-puzzle.txt")
+
+        val displayOutput = Day10.displayOutput(input)
+
+        assertEquals(
+            """
+                ###..####.####.####.#..#.###..####..##..
+                #..#.#.......#.#....#.#..#..#.#....#..#.
+                #..#.###....#..###..##...###..###..#..#.
+                ###..#.....#...#....#.#..#..#.#....####.
+                #.#..#....#....#....#.#..#..#.#....#..#.
+                #..#.#....####.####.#..#.###..#....#..#.
+            """.trimIndent(),
+            displayOutput
+        )
     }
 }
