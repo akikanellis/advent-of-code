04bd74ddeae8b3e81b49539e110861a512cfb873
Aki Kanellis <hello@akikanellis.com>
2023-02-20 20:24:27 +0000

    feat: solve year 2022, day 12 (#33)
    Closes: #28

---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day12Test.kt
@@ -0,0 +1,27 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.utils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+
+class Day12Test {
+    @ParameterizedTest
+    @CsvSource(
+        "/day-12-input-example.txt, true,  31",
+        "/day-12-input-puzzle.txt,  true,  440",
+        "/day-12-input-example.txt, false, 29",
+        "/day-12-input-puzzle.txt,  false, 439"
+    )
+    fun `finds shortest path steps`(
+        inputFile: String,
+        ascending: Boolean,
+        expectedSteps: Int
+    ) {
+        val input = resourceText(inputFile)
+
+        val shortestPathSteps = Day12.shortestPathSteps(input, ascending)
+
+        assertEquals(expectedSteps, shortestPathSteps)
+    }
+}
