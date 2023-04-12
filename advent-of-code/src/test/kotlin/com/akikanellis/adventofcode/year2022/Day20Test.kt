9290e951793c159ce4149e9e6a549a5c306232a3
Aki Kanellis <hello@akikanellis.com>
2023-04-12 16:43:21 +0100

    feat: solve year 2022 day 20

---
+++ advent-of-code/src/test/kotlin/com/akikanellis/adventofcode/year2022/Day20Test.kt
@@ -0,0 +1,40 @@
+package com.akikanellis.adventofcode.year2022
+
+import com.akikanellis.adventofcode.testutils.resourceText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+
+class Day20Test {
+    @ParameterizedTest
+    @CsvSource(
+        "/day-20-input-example.txt, 3",
+        "/day-20-input-puzzle.txt,  4_267"
+    )
+    fun `calculates the sum of grove coordinates with the wrong decryption routine`(
+        inputFile: String,
+        expectedSum: Long
+    ) {
+        val input = resourceText(inputFile)
+
+        val sumOfGroveCoordinates = Day20.sumOfGroveCoordinatesWithWrongDecryptionRoutine(input)
+
+        assertEquals(expectedSum, sumOfGroveCoordinates)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "/day-20-input-example.txt, 1_623_178_306",
+        "/day-20-input-puzzle.txt,  6_871_725_358_451"
+    )
+    fun `calculates the sum of grove coordinates with the correct decryption routine`(
+        inputFile: String,
+        expectedSum: Long
+    ) {
+        val input = resourceText(inputFile)
+
+        val sumOfGroveCoordinates = Day20.sumOfGroveCoordinatesWithCorrectDecryptionRoutine(input)
+
+        assertEquals(expectedSum, sumOfGroveCoordinates)
+    }
+}
